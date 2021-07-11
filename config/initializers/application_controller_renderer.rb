c9a8a955fd10e99b3d4223eaaabcb291c4ff8d8f
M-Abe47 <abuzhengze5@gmail.com>
2021-07-11 09:09:23 +0000

    Add post_function

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5be7086820ac2866b5dc73a9292562d289ed2b733b48df4017e32ab2dca4b20e8c2e52a52f77cd177f61ef546b7331eb3072c14bbfb277d87e144344d9426c9c'
