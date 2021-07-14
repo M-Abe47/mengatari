7ab2429cfcecb8ac28c5300ce7bc39347ee5c957
M-Abe47 <abuzhengze5@gmail.com>
2021-07-14 07:37:07 +0000

    [Add]search_function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,10 @@ class PostsController < ApplicationController
     redirect_to posts_path
   end
 
+  def search
+    @posts = Post.search(params[:keyword])
+  end
+
   private
 
   def post_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update]
   resources :posts do
     resource :favorites, only: [:create, :destroy]
+    collection do
+      get 'search'
+    end
   end
   root to: 'homes#top'
 end
