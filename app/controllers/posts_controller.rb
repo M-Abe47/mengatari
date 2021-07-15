3a272b310b77c487d73314883a2e6244caddce90
M-Abe47 <abuzhengze5@gmail.com>
2021-07-15 10:42:00 +0000

    Add evaluation function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,6 +32,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :image, :body)
+    params.require(:post).permit(:title, :image, :body, :evaluation)
   end
 end
