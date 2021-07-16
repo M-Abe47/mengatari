5e21bfa5726ffa1255c5cd3b36b9c262dbd15e76
M-Abe47 <abuzhengze5@gmail.com>
2021-07-16 12:31:04 +0000

    Add favorite ranking

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :posts, dependent: :destroy
 	has_many :favorites, dependent: :destroy
+	has_many :favorited_posts, through: :favorites, source: :post
   has_many :post_comments, dependent: :destroy
-  
+
   def already_favorited?(post)
     self.favorites.exists?(post_id: post.id)
   end
