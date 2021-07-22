ff18bc65b802b665dd9d3741b68bca458c361ab5
M-Abe47 <abuzhengze5@gmail.com>
2021-07-22 07:52:16 +0000

    一覧レイアウト修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_20_100225) do
-
-  create_table "events", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "start_time"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2021_07_21_115252) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
