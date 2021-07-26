9e701120a8e4cbe95057bc24d37cd7c4d78b9079
M-Abe47 <abuzhengze5@gmail.com>
2021-07-26 13:57:36 +0000

    home、フォーム等のレイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+          sessions: 'users/sessions'
+        }
+
   resources :users, only: [:show, :edit, :update] do
     resources :memos
   end
-  
+
   resources :posts do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
